2eee9ced4c76696c39ad574156121bc991a6b12a
letycialinda <167372919+letycialinda@users.noreply.github.com>
2024-09-05 01:20:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let anoAtual = 2024;
+const nomeCompleto = "Letycia Coelho";
+let idade = 17;
+const cursoFaculdade = "Medicina";
+const cidadeAtual = "SJP";
+
+console.log('olá eu sou a ${ Letycia Coelho}, eu tenho ${17} anos, e quero estar cursando ${Medicina} 
+
+anoAtual = 2025;
+
+console.log(anoAtual);
